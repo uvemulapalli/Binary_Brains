d5afea0da9f7b965684157483a0d33168e9ceccc
TPPrathiba <127833351+TPPrathiba@users.noreply.github.com>
2023-03-23 17:45:42 +0530

    Update main.py

--- python/DerivativePricer/main.py
+++ python/DerivativePricer/main.py
@@ -1,53 +1,80 @@
-from Trade import Trade;
-from Range import Range;
-import Constants;
-from BumpScenarios import BumpScenarios;
-import random;
-import json;
+fimport yfinance as yf
+import pandas as pd
+import json
+from pymongo import MongoClient;
 
-tradeList = [];
-random.seed(3);
 
-                
-for n in range(1,100):
-    multiplier = random.random() + 1;
-    ID = "TID_"+ str(n);
-    myTrade = Trade(ID,40*multiplier,40*multiplier,multiplier,0.08*multiplier,0.3*multiplier)
-    tradeList.append(myTrade);
+def options_chain(symbol):
+
+    tk = yf.Ticker(symbol)
+    # Expiration dates
+    exps = tk.options
+
+    # Get options for each expiration
+    options = pd.DataFrame()
+    for e in exps:
+        opt = tk.option_chain(e)
+        opt = pd.DataFrame().append(opt.calls).append(opt.puts)
+        opt['expirationDate'] = e
+        options = options.append(opt, ignore_index=True)
+        
+  
+    # Boolean column if the option is a CALL
+    options['CALL'] = options['contractSymbol'].str[4:].apply(
+        lambda x: 1 if (x == "C") else 0)
     
-scenario_spot           = BumpScenarios(Constants.RF_SPOT, Constants.BUMP_TYPE_PCT, Range(-10.0, 10.0, 1.0));
-scenario_strike         = BumpScenarios(Constants.RF_STRIKE, Constants.BUMP_TYPE_PCT, Range(-10.0, 10.0, 1.0));
-scenario_intrest_rate   = BumpScenarios(Constants.RF_IR, Constants.BUMP_TYPE_PCT, Range(-1.0, 1.0, 0.1));
-scenario_intrest_vol    = BumpScenarios(Constants.RF_VOL, Constants.BUMP_TYPE_PCT, Range(-10.0, 10.0, 1.0));
-scenario_ttm            = BumpScenarios(Constants.RF_TTM, Constants.BUMP_TYPE_PCT, Range(-99.0, -1.0, 5.0));
+    options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
+    options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate the midpoint of the bid-ask
+    options.drop('lastTradeDate',axis=1,inplace=True);
+    options.drop('inTheMoney',axis=1,inplace=True);
+    options.fillna(value = 0,inplace = True);
+  
+    return options;
 
-print(scenario_spot.display());
-s = scenario_spot.getBumpedValue(10);
 
-bumpedTrades = [];
+symlist = ['AAPL','IBM','MSFT','GOOGL'];
 
-for t in tradeList:
-    print("Bumping " + t.tradeId);
-    bumpedTrades.append(t);
-    list1 = scenario_spot.getBumpedTrades(t);
-    list2 = scenario_strike.getBumpedTrades(t);
-    list3 = scenario_intrest_rate.getBumpedTrades(t);
-    list4 = scenario_intrest_vol.getBumpedTrades(t);
-    list5 = scenario_ttm.getBumpedTrades(t);
-    bumpedTrades.extend(list1);
-    bumpedTrades.extend(list2);
-    bumpedTrades.extend(list3);
-    bumpedTrades.extend(list4);
-    bumpedTrades.extend(list5);
-    
-with open('TradeData.txt', 'w') as f:
-    for x in bumpedTrades:
-        result = json.dumps(x.__dict__);
-        f.write(result);
-        f.write("\n");
+optList = [];
+
+for symbol in symlist:
+    records = options_chain(symbol).to_dict(orient = 'records');
+    optList.extend(records);
+   
+
+print("**********************************************************************")
+
+json_file = "TradeData.json";
+f = open(json_file, 'w');
+
+for item in optList:
+    f.write(str(item) + '\n')
 f.close();
 
-file = open('TradeData.txt', 'r');
-record = file.read();
-print(record);
-file.close();
+f = open(json_file);
+contents = f.read().replace('\'','\"');
+f.close();
+
+f = open(json_file,'w');
+f.write(contents);
+f.close();
+
+myclient = MongoClient("mongodb://21af924e8e2c.mylabserver.com:8080/")
+  
+# database & collection
+db = myclient["TradeData"];
+Collection = db["Options"];
+
+data = [];
+data = [json.loads(line)
+        for line in open(json_file, 'r', encoding='utf-8')];
+
+     
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+if isinstance(data, list):
+    Collection.insert_many(data) 
+else:
+    Collection.insert_one(data);
+
+print(Collection.name);
