97ff8d003b16fef27938e3dd897d788205a6d116
TPPrathiba <127833351+TPPrathiba@users.noreply.github.com>
2023-03-25 21:54:33 +0530

    Update Instruments.py

--- python/aad_sample/Instruments.py
+++ python/aad_sample/Instruments.py
@@ -47,7 +47,27 @@ def loadOptions():
     count = collection.estimated_document_count();
     return jsonify({'Options Loaded': count})
         
- 
+
+@app.route('/getOption', methods=['get'])
+def getInstrumentDetails():
+    symbol = request.args.get('ticker', None)
+    optSymbol = request.args.get('contractSymbol', None)
+    inst = yf.Ticker(symbol)
+    price = inst.fast_info.last_price;
+    
+    expiry = optSymbol[-15:-9];
+    expiry = date(2000+int(expiry[0:2]),int(expiry[2:4]),int(expiry[4:6])).strftime(('%Y-%m-%d'))
+    opt = inst.option_chain(expiry).calls
+    opt['expirationDate'] = expiry
+    opt['spotPrice'] = price
+    opt.drop(columns=['change','volume','openInterest','lastTradeDate','bid','ask','inTheMoney','percentChange','currency','contractSize'],axis=1,inplace=True);
+    opt.rename(columns = {'impliedVolatility':'volatility'}, inplace = True)
+    opt.rename(columns = {'strike':'strikePrice'}, inplace = True)
+    opt['ticker'] = symbol
+    query = "contractSymbol == '{}'".format(optSymbol)
+    x = opt.query(query).to_dict("records")
+  
+    return jsonify(x)
   
 # driver function
 if __name__ == '__main__':  
